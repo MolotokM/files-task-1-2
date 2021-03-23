29c475f621a7c5569bb4ad436902473646342781
Mariia Syrykh <mariiasyrykh@Mariias-MacBook-Pro.local>
2021-03-23 10:25:50 +0200

    task 1 with txt file

---
+++ main.py
@@ -0,0 +1,22 @@
+from StdSuites import lines
+
+#TASK 1 WITH FILES
+def replace_string():
+    with open("/Users/mariiasyrykh/Desktop/test.txt", "r") as my_file:
+        oldtext = my_file.read()
+        print('MY OLD VALUES IN THE TXT FILE', oldtext)
+    replacetext = oldtext.replace('Hello', 'Bye')
+    with open("/Users/mariiasyrykh/Desktop/test.txt", "w") as my_file2:
+        my_file2.write(replacetext)
+        print('MY NEW VALUES IN THE TXT FILE', replacetext)
+replace_string()
+def del_by_index():
+    with open("/Users/mariiasyrykh/Desktop/test.txt") as my_file3:
+        lines = my_file3.readlines()
+        del lines[1]
+        my_file4 = open("/Users/mariiasyrykh/Desktop/test.txt", "w+")
+        for line in lines:
+            my_file4.write(line)
+            print("DELETE BY INDEX 1:", lines)
+            break
+del_by_index()
