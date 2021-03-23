8fae0ab33591a7ffca33c1e1a3b3daa03a164841
Mariia Syrykh <mariiasyrykh@Mariias-MacBook-Pro.local>
2021-03-23 23:09:48 +0200

    task1 was updated, task 2 was done

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from StdSuites import lines
-
 #TASK 1 WITH FILES
 def replace_string():
     with open("/Users/mariiasyrykh/Desktop/test.txt", "r") as my_file:
         oldtext = my_file.read()
-        print('MY OLD VALUES IN THE TXT FILE', oldtext)
+        print('MY OLD VALUES IN THE TXT FILE: ', oldtext)
     replacetext = oldtext.replace('Hello', 'Bye')
     with open("/Users/mariiasyrykh/Desktop/test.txt", "w") as my_file2:
         my_file2.write(replacetext)
-        print('MY NEW VALUES IN THE TXT FILE', replacetext)
+        print('MY NEW VALUES IN THE TXT FILE: ', replacetext)
 replace_string()
 def del_by_index():
     with open("/Users/mariiasyrykh/Desktop/test.txt") as my_file3:
@@ -17,6 +15,11 @@ def del_by_index():
         my_file4 = open("/Users/mariiasyrykh/Desktop/test.txt", "w+")
         for line in lines:
             my_file4.write(line)
-            print("DELETE BY INDEX 1:", lines)
-            break
+        print("DELETE BY INDEX 1: ", lines)
 del_by_index()
+
+#TASK 2 WITH FILES
+with open("/Users/mariiasyrykh/Desktop/test.txt") as text_file:
+    lines = text_file.readlines()
+    for line in reversed(lines):
+        print (line)
